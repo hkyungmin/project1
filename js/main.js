59ca236032de816c0c6e6452df22186b64080ed1
hkyungmin <imh.kyungmin@gmail.com>
2020-07-15 15:15:45 +0900

    up

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
 $(function(){
-    
+    $(".regular").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+       autoplay : false,			// 자동 스크롤 사용 여부
+        autoplaySpeed : 2000, 		// 자동 스크롤 시 다음으로 넘어가는데 걸리는 시간 (ms)
+      responsive: [                   
+                       {  breakpoint: 1200,    
+                              settings: {
+                                slidesToShow: 3,
+                                slidesToScroll: 1
+                              }
+                         }, {  breakpoint: 1024,
+                              settings: {
+                                slidesToShow: 2,
+                                slidesToScroll: 1
+                              }
+                         },{  breakpoint:1000,
+                              settings: {
+                                slidesToShow: 1,
+                                slidesToScroll: 1
+                              }
+                           }
+                 ]
+      
+      
+      });
     
     $('.bxslider').bxSlider({
             captions:true,
@@ -9,29 +36,9 @@ $(function(){
             stopAutoOnclick:true,
 
           });
-               
-                 
-      var mySlider=$("#slide_banner").bxSlider({
-      mode:"horizontal",   // 가로 수평으로 슬라이드 됩니다.
-      speed:500,              // 이동 속도를 설정합니다.
-      pager:false,            // 현재 위치 페이징 표시 여부 설정.
-      moveSlides:1,         // 슬라이드 이동시 갯수 설정.
-      slideWidth:500,        // 슬라이드 마다 너비 설정.
-      minSlides:4,           // 최소 노출 개수를 설정합니다.
-      maxSlides:4,          // 최대 노출 개수를 설정합니다.
-      slideMargin:20,      // 슬라이드간의 간격을 설정합니다.
-      auto:true,             // 자동으로 흐를지 설정합니다.
-      autoHover:true,    // 마우스 오버시 정시킬지 설정합니다.
-      controls:false,        // 이전 다음 버튼 노출 여부 설정합니다.
-       
-          
-   });
-    
-    
 
-    
-    
-    
+                 
+  
 //이전 버튼을 클릭하면 이전 슬라이드로 전환됩니다.
    $("#prevBtn").on("click",function(){
     mySlider.goToPrevSlide();  //이전 슬라이드 배너로 이동됩니다.
@@ -41,7 +48,10 @@ $(function(){
    $("#nextBtn").on("click",function(){
     mySlider.goToNextSlide();  //다음 슬라이드 배너로 이동됩니다.
     return false;
-   });        
+   }); 
+    
+    
+    
     
     //네비
         $('.mainmenu li').each(function(){
@@ -63,8 +73,6 @@ $(function(){
             }
         })
       
-    
-    
     
     
      $(".open1").click(function(){
@@ -90,8 +98,9 @@ $(function(){
        $('.mo_sub').slideUp();
          $(submenu).stop().slideToggle();
        })
-	})  
-     
+	})
+    
+    
     
     
     

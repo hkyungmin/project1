8e9f5637ac9eb4138f8c81cd3ab6b479afcd341a
hkyungmin <imh.kyungmin@gmail.com>
2020-07-16 10:16:34 +0900

    up

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
-    $(".regular").slick({
-        dots: true,
+ $(".regular").slick({
+        dots: false,
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
@@ -23,10 +23,59 @@ $(function(){
                                 slidesToScroll: 1
                               }
                            }
-                 ]
+                ]
       
       
       });
+    
+    $('.tabSet').each(function(){
+        var anchors = $(this).find('.tabs a');
+        var panelDivs = $(this).find('.panel');
+        var lastAnchor;
+        var lastPanel;
+        
+        anchors.show();//
+        lastAnchor = anchors.filter('.on');
+        lastPanel = $(lastAnchor.attr('tab_href'));
+        
+        var lastImg = $(this).find('.tabs .on img'); //lastAnchor.find('img');
+        var lastImgsrc = lastImg.attr('src');
+        
+        
+        panelDivs.hide();
+        lastPanel.show();
+        
+        anchors.click(function(){
+          var currentAnchor = $(this);
+          var currentPanel = $(currentAnchor.attr('tab_href'));
+          var currentImg = $(this).find('img');
+          var currentImgsrc =  currentImg.attr('src');
+          var currentImgsrcOn = currentImgsrc.replace('off','on');
+          var lastImgsrcOff = lastImgsrc.replace('on','off')  ;
+            
+           
+           currentAnchor.addClass('on');
+           lastAnchor.removeClass('on');
+           lastAnchor =  currentAnchor;
+           
+           currentImg.attr('src',currentImgsrcOn);
+           lastImg.attr('src',lastImgsrcOff);
+           //현재를 과거로 기억시킴
+           lastImg = currentImg;
+           lastImgsrc = lastImg.attr('src');
+ 
+           
+            
+           currentPanel.show();
+           lastPanel.hide();   
+           lastPanel = currentPanel;
+        })
+        
+    })
+    
+    
+     
+    
     
     $('.bxslider').bxSlider({
             captions:true,
@@ -65,13 +114,13 @@ $(function(){
             //hover는 펑션이 두번 가능함
         })//nav li each
         
-        $(window).scroll(function(){
+        /*$(window).scroll(function(){
             if($(document).scrollTop() >= 300) {
                 $('header').css({background:'rgba(0,0,0,0.5)'});
             }else{
                 $('header').css({background:'rgba(0,0,0,0)'});
             }
-        })
+        })*/
       
     
     
@@ -100,6 +149,28 @@ $(function(){
        })
 	})
     
+    
+    
+    $(window).resize(function(){    location.reload();         
+   if ($(window).width() > 740) {
+       
+        $(".regular").slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1
+      });
+    }else{
+        
+     $(".regular").slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      });    
+    }
+ 
+})
     
     
     

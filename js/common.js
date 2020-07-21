1de560a5dcb2e982bb337d911ceb40fbd144cade
hkyungmin <imh.kyungmin@gmail.com>
2020-07-21 17:28:17 +0900

    up

--- js/common.js
+++ js/common.js
@@ -58,14 +58,53 @@ $(function(){
     
        
     
-  $(window).resize(function(){    window.location.reload();        
-   if ($(window).width() > 740) {
-      
-  
-    }else{
-         
-
-    }
+  $(window).resize(function(){           
+    $('.tabSet').each(function(){
+        var anchors = $(this).find('.tabs a');
+        var panelDivs = $(this).find('.panel');
+        var lastAnchor;
+        var lastPanel;
+        
+        anchors.show();//
+        lastAnchor = anchors.filter('.on');
+        lastPanel = $(lastAnchor.attr('tab_href'));
+        
+        var lastImg = $(this).find('.tabs .on img'); //lastAnchor.find('img');
+        var lastImgsrc = lastImg.attr('src');
+        
+        
+        panelDivs.hide();
+        lastPanel.show();
+        
+        anchors.click(function(){
+          var currentAnchor = $(this);
+          var currentPanel = $(currentAnchor.attr('tab_href'));
+          var currentImg = $(this).find('img');
+          var currentImgsrc =  currentImg.attr('src');
+          var currentImgsrcOn = currentImgsrc.replace('off','on');
+          var lastImgsrcOff = lastImgsrc.replace('on','off')  ;
+            
+           
+           currentAnchor.addClass('on');
+           lastAnchor.removeClass('on');
+           lastAnchor =  currentAnchor;
+           
+           currentImg.attr('src',currentImgsrcOn);
+           lastImg.attr('src',lastImgsrcOff);
+           //현재를 과거로 기억시킴
+           lastImg = currentImg;
+           lastImgsrc = lastImg.attr('src');
+ 
+           
+            
+           currentPanel.show();
+           lastPanel.hide();   
+           lastPanel = currentPanel;
+        })
+        
+    })
+    
+    
  
 })
                  

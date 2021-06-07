da457bd6b28a8f5580cce9f89c5664421a819b40
hkyungmin <68043349+hkyungmin@users.noreply.github.com>
2021-06-07 19:51:45 +0900

    up

--- js/slider.js
+++ js/slider.js
@@ -4,7 +4,7 @@
  var mySlider=$("#slide_banner").bxSlider({
       mode:"horizontal",   // 가로 수평으로 슬라이드 됩니다.
       speed:500,              // 이동 속도를 설정합니다.
-      pager:true,            // 현재 위치 페이징 표시 여부 설정.
+      pager:false,            // 현재 위치 페이징 표시 여부 설정.
       moveSlides:1,         // 슬라이드 이동시 갯수 설정.
       slideWidth:500,        // 슬라이드 마다 너비 설정.
       minSlides:2,           // 최소 노출 개수를 설정합니다.
